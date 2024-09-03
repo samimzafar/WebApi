2fb6c3130d09c0f16e967e79ddec51030d8ef05a
sameem.z@dplit.com-at-401940162791 <sameem.z@dplit.com>
2024-09-03 11:23:29 +0500

    migrations file remove from gitignore and updated the files

---
+++ Migrations/20240903061044_InitialCreate.cs
@@ -0,0 +1,155 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace WebApi.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Admins",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Username = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Password = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Admins", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Schools",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Location = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Fees = table.Column<decimal>(type: "decimal(18,2)", nullable: false),
+                    Open = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Schools", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Transports",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    BusNumber = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    SchoolId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Transports", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Transports_Schools_SchoolId",
+                        column: x => x.SchoolId,
+                        principalTable: "Schools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Address = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Age = table.Column<int>(type: "int", nullable: false),
+                    SchoolId = table.Column<int>(type: "int", nullable: false),
+                    TransportId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Schools_SchoolId",
+                        column: x => x.SchoolId,
+                        principalTable: "Schools",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                    table.ForeignKey(
+                        name: "FK_Students_Transports_TransportId",
+                        column: x => x.TransportId,
+                        principalTable: "Transports",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.NoAction);
+                });
+
+            migrationBuilder.InsertData(
+                table: "Admins",
+                columns: new[] { "Id", "Password", "Username" },
+                values: new object[] { 1, "1234", "admin" });
+
+            migrationBuilder.InsertData(
+                table: "Schools",
+                columns: new[] { "Id", "Fees", "Location", "Name", "Open" },
+                values: new object[,]
+                {
+                    { 1, 1000m, "Sawat", "APS", true },
+                    { 2, 1500m, "Kohat", "IQRA", true },
+                    { 3, 1200m, "Mardan", "BIRD SKY", true },
+                    { 4, 1100m, "Swabi", "CITY LEARN", true },
+                    { 5, 1300m, "Lahore", "WALKSCHOOL", true }
+                });
+
+            migrationBuilder.InsertData(
+                table: "Transports",
+                columns: new[] { "Id", "BusNumber", "SchoolId" },
+                values: new object[,]
+                {
+                    { 1, "Bus101", 1 },
+                    { 2, "Bus102", 2 },
+                    { 3, "Bus103", 3 },
+                    { 4, "Bus104", 4 },
+                    { 5, "Bus105", 5 }
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_SchoolId",
+                table: "Students",
+                column: "SchoolId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_TransportId",
+                table: "Students",
+                column: "TransportId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Transports_SchoolId",
+                table: "Transports",
+                column: "SchoolId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Admins");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Transports");
+
+            migrationBuilder.DropTable(
+                name: "Schools");
+        }
+    }
+}
